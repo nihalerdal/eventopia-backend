d4effcf3feaf70170aac80938384a6a5cb67baa0
nihalerdal <nihalerdall@gmail.com>
2025-10-31 17:36:58 -0700

    update port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config({ path: "../.env" });
-const { PORT = 8000 } = process.env;
+const PORT = process.env.PORT || 8000;
 const app = require("./app");
 const connectDB = require("./db/connect");
 
